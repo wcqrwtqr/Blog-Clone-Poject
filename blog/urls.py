c2757d500fd0ccb9be9abb217578d81f740f503e
wcqrwtqr <mohalbatati@hotmail.com>
2020-09-15 23:13:36 +0300

    Updated the view.py with all the needed CRUD view, added index,base, about html pages

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from blog import views
+
+
+
+urlpatterns = [
+
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DetailView
+from django.contrib.auth.decorators import login_required
+from blog.models import Post, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+from blog.forms import PostForm, CommentForm
+from django.urls import  reverse_lazy
 # Create your views here.
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')) # TOCHECK this is very possible to be wrong
+    # for more details we can refer to the Field lookups in django  - Documentation
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin,CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html' # TOCHECK
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html' # TOCHECK
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DetailView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(ListView, LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html' # TOCHECK the link later if its working or not
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+
+
+
+
