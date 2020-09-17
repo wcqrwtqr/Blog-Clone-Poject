2264e4c4536f3a7899183658fd1a9a448cd930f6
wcqrwtqr <mohalbatati@hotmail.com>
2020-09-18 00:32:19 +0300

    updated the urls.py and views.py with all the needed viewds for updated, remove, publish etc

--- blog/urls.py
+++ blog/urls.py
@@ -12,5 +12,9 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post.as_view(), name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approved.as_view(), name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove.as_view(), name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish.as_view(), name='post_publish'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
+from django.utils import  timezone
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DetailView
 from django.contrib.auth.decorators import login_required
 from blog.models import Post, Comment
@@ -47,6 +48,42 @@ class DraftListView(ListView, LoginRequiredMixin):
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
+
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            Comment = form.save(commit=False)
+            Comment.post = post
+            Comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blob/comment_form.html', {'form':form})
+
+
+@login_required
+def comment_approved(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approved()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
 
 
 
